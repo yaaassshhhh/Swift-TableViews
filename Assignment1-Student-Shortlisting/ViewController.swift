14a11bd52f3536bd4a627c059018f929fea56f22
yash.agrawal <yash.agrawal@bookmyshow.com>
2025-06-11 15:58:25 +0530

    Initial Commit

---
+++ Assignment1-Student-Shortlisting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment1-Student-Shortlisting
+//
+//  Created by Yash Agrawal on 11/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
